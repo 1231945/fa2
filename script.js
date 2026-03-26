bb16729cd01d16a49e010c987243e19f73df8d72
1231945 <zivandrei.calonzo@smc.pshs.edu.ph>
2026-03-26 11:22:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// function to calculate subtotal using service and sessions
+function calculateSubtotal(service, sessions) {
+    switch(service) {
+        case 'Dental Check-up (PHP 850)':
+            return 850 * sessions;
+        case 'Eye Check-up (PHP 900)':
+            return 900 * sessions;
+        case 'Physical Exam (PHP 800)':
+            return 800 * sessions;
+        default:
+            return 0;
+    }
+}
+
+// function to calculate discount when applicable
+function calculateDiscount(subtotal, customer) {
+    if (customer === "pwd/senior") {
+        return subtotal * 0.15;
+    } else {
+        return 0;
+    }
+}
+
+function printSummary() {
+    // get values from form
+    const name = document.getElementById('name').value;
+    const id = document.getElementById('id').value;
+    const service = document.getElementById('service').value;
+    const sessions = parseInt(document.getElementById('sessions').value);
+    const customer = document.getElementById('customer').value;
+
+    // calculate subtotal and discount
+    const subtotal = calculateSubtotal(service, sessions);
+    const discount = calculateDiscount(subtotal, customer);
+ 
+    
+    // print on html
+    document.getElementById('nameO').innerText = "Name: " + name;
+    document.getElementById('idO').innerText = "Grade and Section " + id;
+    document.getElementById('Order').innerText = "Order: " + service;
+    document.getElementById('Quantity').innerText = "Quantity: " + sessions;
+    document.getElementById('subtotal').innerText = "Subtotal: " + subtotal;
+    document.getElementById('discount').innerText = "Discount: " + discount;
+    document.getElementById('total').innerText = "Total: " + (subtotal - discount);
+    document.getElementById('msg').innerText = "Thank you for booking with us!";
+}
+
